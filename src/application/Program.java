68e9e62f2fa3b1fbf190f580fa4b528f900ef6d6
The John! <jdworks1998@gmail.com>
2022-10-27 23:33:00 -0300

    Seller Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,6 +12,10 @@ public class Program {
 		//Test
 		Department dp = new Department(1, "seller");
 		System.out.println(dp);
+		
+		Seller seller = new Seller(21, "Bob","bobzola@gmail.com", new Date(), 3000.0, dp);
+		
+		System.out.println(seller);
 	}
 
 }
